fbbcc52ae559c757264e6e7c1bdf45a3ef1d7785
Artem <mikhnyukartemm@gmail.com>
2024-09-11 21:10:30 +0200

    Commit Message

---
+++ src/main/kotlin/org/mikhnyukartemm/kotlincourse/lesson3/Homework.kt
@@ -0,0 +1,27 @@
+package org.mikhnyukartemm.kotlincourse.lesson3
+
+val name: String = "День рождения" // Название мероприятия
+
+val date: String = "09.10.2024" // Дата
+
+val place: String = "Sochi, St.Ivanovskaya 125" // Адресс
+
+var budget: Double = 0.0 // Бюджет
+
+var member: Int = 10 // Число участников может изминиться, кто-то не придет
+
+var duration: String = "8 hours"  //Длительность хаккатона может измениться по стечению обстоятельств
+
+var contactinformation: String = "Имя,Почта,Номер"  // Контактная информация
+
+var status: String = "Планируется"   //  Статус мероприятия
+
+var sponsors: String = "Спонсор 1 "  //Мне кажется, что список спонсоров может меняться в какихто случаях
+
+var Internetaccesslevel: String = "Высокий" // Уровень интернета может быть разым
+
+
+
+
+
+
