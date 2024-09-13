0b0da0ab044b0ae577b7e3bf9b5d31fc537f066d
Artem <mikhnyukartemm@gmail.com>
2024-09-13 16:24:41 +0200

    Commit Message

--- src/main/kotlin/org/mikhnyukartemm/kotlincourse/lesson3/Homework.kt
+++ src/main/kotlin/org/mikhnyukartemm/kotlincourse/lesson3/Homework.kt
@@ -8,17 +8,45 @@ val place: String = "Sochi, St.Ivanovskaya 125" // Адресс
 
 var budget: Double = 0.0 // Бюджет
 
-var member: Int = 10 // Число участников может изминиться, кто-то не придет
+var member: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) field = value
+        else field  = 50
+    }      // Число участников может изминиться, кто-то не придет
 
 var duration: String = "8 hours"  //Длительность хаккатона может измениться по стечению обстоятельств
 
-var contactinformation: String = "Имя,Почта,Номер"  // Контактная информация
+lateinit var contactinformation: String   // Контактная информация будет позднее
 
-var status: String = "Планируется"   //  Статус мероприятия
+lateinit var status: String    //  Статус мероприятия
 
-var sponsors: String = "Спонсор 1 "  //Мне кажется, что список спонсоров может меняться в какихто случаях
+ var sponsors: String = "список спонсоров" //Мне кажется, что список спонсоров может меняться в какихто случаях
 
 var Internetaccesslevel: String = "Высокий" // Уровень интернета может быть разым
+
+lateinit var mood: String  // Настроение
+
+val Information: String = "Информация о транспартировке"; //Информация о транспортировке оборудования, распределении ресурсов и координации между различными командами поддержки.
+
+val plan: String by lazy { "09:00, Встреча гостей"  } //Подробный план хакатона, включающий время и содержание каждого сегмента, инициализируется непосредственно перед началом мероприятия
+
+lateinit var specialGuest: String // Специальный гость который будет обьявлен перед началом мероприятия
+
+var Numberofcommands: Int = 0
+    get() = field
+    set(value) {
+        if (value >= 0) field = value
+        else field  = 5
+    }  //Количество команд
+
+val tasks: String = "Задачи" //Перечень задач
+
+var Evacuationplan: String = "План эвакуации" //План эвакуации
+
+lateinit var Listofavailableequipment: String //Список доступного оборудования
+
+lateinit var Listoffreeequipment: String //Список свободного оборудования
 
 
 
