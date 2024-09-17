dbe4a470b9f95a6d0fae51e260c2a7428b284117
Artem <mikhnyukartemm@gmail.com>
2024-09-17 19:00:36 +0200

    Commit

---
+++ src/main/kotlin/org/mikhnyukartemm/kotlincourse/lesson5/lesson5.kt
@@ -0,0 +1,27 @@
+package org.mikhnyukartemm.kotlincourse.lesson5
+
+fun main(){
+    val sum = "5" + "3"
+    val dif = 10 - 5
+    val quotient = 10 * 5
+    val remainder = 10 % 5
+    val remainder2 = 10 % 6
+    val isEqual = 5 == 4
+    val isNotEqual = (5 != 5)
+    val isGreater = (5 > 3)
+    val isLesser = (5 < 3)
+    val isGreaterOrEqual = (5 >= 3)
+    val isLesserOrEqual = (5 <= 3)
+    val andResult = false && false
+    val orResult = true || false
+    val notResult = !(5 > 3)
+    var number = 5
+    number += 3
+    number /= 2
+
+
+
+
+    println(notResult)
+    println(number)
+}
